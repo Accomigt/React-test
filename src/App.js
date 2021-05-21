f64df143c23d8193eaca30fb1cf8f70b9e9f845e
Accomigt <accomigt@gmail.com>
2021-05-21 15:21:26 -0500

    spanish

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Aplicacion de react</h1>
+        <h1>Prueba de aplicacion de React</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
